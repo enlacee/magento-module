f76b9b5f5c73d8af1a8ada3162a18b2e00ad1114
Anibal Copitan <anibal.copitan@altimea.com>
2017-05-09 18:40:03 -0500

    update: agregar menu en admin

---
+++ app/code/local/Altimea/Scheduledtasks/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Altimea_Scheduledtasks_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
--- app/code/local/Altimea/Scheduledtasks/controllers/IndexController.php
+++ app/code/local/Altimea/Scheduledtasks/controllers/IndexController.php
@@ -1,6 +1,19 @@
 <?php
 class Altimea_Scheduledtasks_IndexController extends Mage_Core_Controller_Front_Action {
-    public function indexAction() {
-    	echo "aax";
-    }
+
+	public function indexAction()
+	{
+		$customer = Mage::getModel('customer/customer');
+		$customer->setWebsiteId(1);
+		$dat = $customer->loadByEmail('martha_delgadillo11@hotmail.com');
+
+		// user with email exit
+		if ($customer->getId()) {
+			$customer->setIsDeleteable(true);
+			$customer->delete();
+			echo "true";
+		} else {
+			echo "false";
+		}
+	}
 }
